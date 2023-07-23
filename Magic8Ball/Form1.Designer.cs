8f2492e7da2f0e17415d647b3f2b30a1696b23c7
Fabian <rxs077@gmail.com>
2023-07-23 16:05:33 -0500

    Add project files.

---
+++ Magic8Ball/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Magic8Ball
+{
+    partial class MagicEightBall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MagicEightBall));
+            this.submitButton = new System.Windows.Forms.Button();
+            this.questionBox = new System.Windows.Forms.TextBox();
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.Color.Black;
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.ForeColor = System.Drawing.Color.White;
+            this.submitButton.Location = new System.Drawing.Point(38, 377);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(160, 48);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Tell me!";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // questionBox
+            // 
+            this.questionBox.BackColor = System.Drawing.Color.Black;
+            this.questionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionBox.ForeColor = System.Drawing.Color.White;
+            this.questionBox.Location = new System.Drawing.Point(204, 377);
+            this.questionBox.Multiline = true;
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(564, 48);
+            this.questionBox.TabIndex = 1;
+            this.questionBox.Text = "ASK ME IF YOU DARE!";
+            this.questionBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.answerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerLabel.ForeColor = System.Drawing.Color.White;
+            this.answerLabel.Location = new System.Drawing.Point(412, 183);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(110, 70);
+            this.answerLabel.TabIndex = 2;
+            this.answerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.answerLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MagicEightBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.questionBox);
+            this.Controls.Add(this.submitButton);
+            this.Name = "MagicEightBall";
+            this.Text = "Magic 8 Ball";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox questionBox;
+        public System.Windows.Forms.Label answerLabel;
+    }
+}
+
---
+++ Magic8Ball/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic8Ball
+{
+    public partial class MagicEightBall : Form
+    {
+
+Random random = new Random();
+            List<string> answers = new List<string>();
+            public MagicEightBall()
+            {
+                InitializeComponent();
+
+                /* Yes Answers */
+                answers.Add("It is certain.");
+                answers.Add("It is decidedly so.");
+                answers.Add("Without a doubt.");
+                answers.Add("Yes definitely.");
+                answers.Add("You may rely on it.");
+                /* Middle Ground Answers */
+                answers.Add("As I see it, yes.");
+                answers.Add("Most likely.");
+                answers.Add("Outlook good.");
+                answers.Add("Yes.");
+                answers.Add("Signs point to yes.");
+
+                answers.Add("Reply hazy, try again.");
+                answers.Add("Ask again later.");
+                answers.Add("Better not tell you now.");
+                answers.Add("Cannot predict now.");
+                answers.Add("Concentrate and ask again.");
+                /* No answers */
+                answers.Add("Don't count on it.");
+                answers.Add("My reply is no.");
+                answers.Add("My sources say no.");
+                answers.Add("utlook not so good.");
+                answers.Add("Very doubtful.");
+
+            }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            int index = random.Next(answers.Count);
+            answerLabel.Text = answers[index];
+
+        }
+    }
+}
